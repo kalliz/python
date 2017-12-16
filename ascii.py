642d139619377aa4c66e0ec1ad783a9e0e301575
johnjagan13 <34595251+johnjagan13@users.noreply.github.com>
2017-12-16 14:11:54 +0530

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,13 +1,13 @@
 try:
-	a='abcd'
+	a=str(raw_input())
 	b=len(a)
 	tot=0
 	avg=0
 	sum=0
 	for i in range (0,b):
-	c=ord(a[i])
-	sum=sum+c
-	avg=sum/b
+		c=ord(a[i])
+		sum=sum+c
+		avg=sum/b
 	print chr(avg)
 except:
   	print('invalid')

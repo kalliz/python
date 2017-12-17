15bf99c4c74b99445045d43a67b529ab811c9f65
johnjagan13 <34595251+johnjagan13@users.noreply.github.com>
2017-12-17 23:15:12 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,9 @@
-n=int(raw_input())
-fact=1
-while(n!=0):
-	fact=fact*n
-	n=n-1
-print fact
+try:
+	n=int(raw_input())
+	fact=1
+	while(n!=0):
+		fact=fact*n
+		n=n-1
+	print fact
+except:
+	print "Error"

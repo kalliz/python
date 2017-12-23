54a69aa3c734057c6351b495d108aed4d14c79b9
johnjagan13 <34595251+johnjagan13@users.noreply.github.com>
2017-12-23 21:50:06 +0530

    Update camel_case.py

--- camel_case.py
+++ camel_case.py
@@ -1,9 +1,12 @@
-s1=raw_input()
-a=s1.split(' ')
-g=" "
-for i in range(0,len(a)):
-		a[i]=a[i].capitalize()
-c=a[0]
-for i in range(1,len(a)):
-	c=c+g+a[i]
-print c
+try:
+	s1=raw_input()
+	a=s1.split(' ')
+	g=" "
+	for i in range(0,len(a)):
+			a[i]=a[i].capitalize()
+	c=a[0]
+	for i in range(1,len(a)):
+		c=c+g+a[i]
+	print c
+except:
+	print "Invalid"

c585cdcfe19dd030ae775b5a55ed9d8113a75acf
johnjagan13 <34595251+johnjagan13@users.noreply.github.com>
2017-12-23 09:27:35 +0530

    Update repeat.py

--- repeat.py
+++ repeat.py
@@ -2,18 +2,17 @@ s1=str(raw_input())
 g=str()
 s=s1.split(' ')
 n=len(s)
+a=[]
+for i in range (0,n):
+	a.append(0)
 for i in range(0,n):
 	m=len(s[i])
-	c=0
 	for j in range(0,m):
-		for k in range (1,m):
+		for k in range (0,m):
 			if(j!=k):
-				f=s[i][j]
-				if(f!=1):
 					if(s[i][j]==s[i][k]):
 						g=g+(s[i][j])
-						f=1
-				else:
-					c=c+1
+						a[i]=a[i]+1
 print g
-print c
+print a
+for i in range (0,)

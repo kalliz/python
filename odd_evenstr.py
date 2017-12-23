695e499a0177a098830e3a1e572dc8a6105f72a3
johnjagan13 <34595251+johnjagan13@users.noreply.github.com>
2017-12-23 22:38:54 +0530

    Update odd_evenstr.py

--- odd_evenstr.py
+++ odd_evenstr.py
@@ -2,6 +2,7 @@ try:
 	c=0
 	n=str(raw_input())
 	a=[]
+	m=str()
 	for i in range (0,len(n)):
 		a.append(n[i])
 	if len(n)%2==0:
@@ -13,6 +14,8 @@ try:
 		a[c]=a[c+1]
 		a[c+1]=t
 		c=c+2
-	print a
+	for i in range (0,len(a)):
+		m=m+a[i]
+	print m
 except:
 	print "Invalid"

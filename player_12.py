67b61558603ab05f10e4fb0088fd75af5d6999c6
johnjagan13 <34595251+johnjagan13@users.noreply.github.com>
2018-06-12 09:16:51 +0000

    Update player_12.py

--- player_12.py
+++ player_12.py
@@ -1,14 +1,17 @@
 # your code goes here
-a=[]
-n=int(input())
-m=int(input())
-for i in range(n):
- a.append(int(input()))
-for i in range(m):
- l=len(a)-1
- b=a[l]
- while(l>=0):
-  a[l]=a[l-1]
-  l=l-1
- a[0]=b
-print(a)
+try:
+ a=[]
+ n=int(input())
+ m=int(input())
+ for i in range(n):
+  a.append(int(input()))
+ for i in range(m):
+  l=len(a)-1
+  b=a[l]
+  while(l>=0):
+   a[l]=a[l-1]
+   l=l-1
+  a[0]=b
+ print(a)
+except:
+ print("error")

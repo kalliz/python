3171222ec23d05fd6ec0994d53239e8673c406cb
johnjagan13 <34595251+johnjagan13@users.noreply.github.com>
2018-06-26 17:18:37 +0000

    Update perfect_squre.py

--- perfect_squre.py
+++ perfect_squre.py
@@ -1,11 +1,14 @@
-s=str(input())
-l=s.split(" ")
-min=int(l[0])
-max=int(l[1])
-count=0
-for i in range(min,max+1):
- y=i**(.5)
- z=round(y)
- if(y%z==0):
-  count+=1
-print(count)
+try:
+ s=str(input())
+ l=s.split(" ")
+ min=int(l[0])
+ max=int(l[1])
+ count=0
+ for i in range(min,max+1):
+  y=i**(.5)
+  z=round(y)
+  if(y%z==0):
+   count+=1
+ print(count)
+except:
+ print("Invalid")
